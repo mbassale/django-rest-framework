ab642cd9b350d5205dcfed0c555188f04a743839
Marco Bassaletti <bassaletti@gmail.com>
2020-06-28 14:44:22 -0400

    Added views to list, view, create, edit and delete snippets.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from tutorial.snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from tutorial.snippets.models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
