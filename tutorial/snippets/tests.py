db7902134f58fe101d7fc0b0e79f5703898b32c2
Marco Bassaletti <bassaletti@gmail.com>
2020-06-28 21:54:17 -0400

    Added routes and unit test for list endpoint.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- tutorial/snippets/tests.py
+++ tutorial/snippets/tests.py
@@ -4,6 +4,7 @@ from tutorial.snippets.serializers import SnippetSerializer
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework import status
 from django.urls import reverse
 from pprint import pprint
 from faker import Faker
@@ -54,3 +55,39 @@ class SnippetSerializerTest(APITestCase):
         updated_snippet = serializer.save()
         self.assertEquals(updated_snippet.title, new_title)
         self.assertEquals(updated_snippet.code, new_code)
+
+
+class SnippetViewTest(APITestCase):
+
+    faker = Faker()
+
+    snippet_count = 0
+
+    def new_snippet_data(self):
+        title = 'Snippet ' + str(self.snippet_count + 1)
+        code = 'print("Hi, World!")'
+        linenos = self.faker.pybool()
+        language = 'python'
+        style = 'friendly'
+        return title, code, linenos, language, style
+
+    def test_snippet_list(self):
+        for _ in range(10):
+            (title, code, linenos, language, style) = self.new_snippet_data()
+            snippet = Snippet(title=title, code=code, linenos=linenos, language=language, style=style)
+            snippet.save()
+        client = APIClient()
+        response = client.get(reverse('snippet-list'))
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response)
+        self.assertTrue(len(response.content) > 0)
+        stream = BytesIO(response.content)
+        data = JSONParser().parse(stream)
+        self.assertTrue(len(data) == 10)
+        for datum in data:
+            self.assertTrue('id' in datum)
+            snippet = Snippet.objects.get(pk=datum['id'])
+            self.assertIsNotNone(snippet)
+            self.assertEqual(snippet.id, datum['id'])
+            self.assertEqual(snippet.title, datum['title'])
