2be1091d90ddea7b9079e3e0289b29f67ed7bb67
Marco Bassaletti <bassaletti@gmail.com>
2020-07-04 14:12:19 -0400

    Owner read only field for snippet entity.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- tutorial/snippets/tests.py
+++ tutorial/snippets/tests.py
@@ -1,8 +1,7 @@
 from io import BytesIO
+from django.contrib.auth.models import User
 from tutorial.snippets.models import Snippet
-from tutorial.snippets.serializers import SnippetSerializer
 from rest_framework.test import APITestCase, APIClient
-from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from django.urls import reverse
@@ -10,58 +9,24 @@ from pprint import pprint
 from faker import Faker
 
 
-class SnippetSerializerTest(APITestCase):
-    faker = Faker()
-
-    def test_serialize(self):
-        # test serialization
-        snippet = Snippet(code='print("Hello, world")\n')
-        snippet.save()
-        self.assertEquals(Snippet.objects.count(), 1)
-        serializer = SnippetSerializer(snippet)
-        content = serializer.data
-        self.assertIsNotNone(content)
-        self.assertTrue('id' in content and
-                        'title' in content and
-                        'code' in content and
-                        'language' in content)
-
-        # convert to json
-        content = JSONRenderer().render(serializer.data)
-        self.assertIsNotNone(content)
-
-        # test deserialization and creation
-        stream = BytesIO(content)
-        data = JSONParser().parse(stream)
-        serializer = SnippetSerializer(data=data)
-        self.assertTrue(serializer.is_valid())
-        self.assertTrue('title' in serializer.validated_data and
-                        'code' in serializer.validated_data and
-                        'language' in serializer.validated_data)
-        new_snippet = serializer.save()
-        self.assertIsNotNone(new_snippet)
-        self.assertNotEqual(snippet.id, new_snippet.id)
-
-        new_title = 'Updated code'
-        new_code = 'print("Hi, World!")'
-        new_snippet.title = new_title
-        new_snippet.code = new_code
-        serializer = SnippetSerializer(new_snippet)
-        new_content = JSONRenderer().render(serializer.data)
-        stream = BytesIO(new_content)
-        data = JSONParser().parse(stream)
-        serializer = SnippetSerializer(new_snippet, data=data)
-        self.assertTrue(serializer.is_valid())
-        updated_snippet = serializer.save()
-        self.assertEquals(updated_snippet.title, new_title)
-        self.assertEquals(updated_snippet.code, new_code)
-
-
 class SnippetViewTest(APITestCase):
 
     faker = Faker()
 
+    user = None
+
     snippet_count = 0
+
+    def setUp(self):
+        first_name = self.faker.first_name()
+        last_name = self.faker.last_name()
+        username = first_name[0].lower() + last_name.lower()
+        email = '{}@{}'.format(username, self.faker.domain_name())
+        self.user = User(first_name=first_name,
+                         last_name=last_name,
+                         username=username,
+                         email=email)
+        self.user.save()
 
     def new_snippet_data(self):
         title = 'Snippet ' + str(self.snippet_count + 1)
@@ -74,7 +39,7 @@ class SnippetViewTest(APITestCase):
     def test_snippet_list(self):
         for _ in range(10):
             (title, code, linenos, language, style) = self.new_snippet_data()
-            snippet = Snippet(title=title, code=code, linenos=linenos, language=language, style=style)
+            snippet = Snippet(owner_id=self.user.id, title=title, code=code, linenos=linenos, language=language, style=style)
             snippet.save()
         client = APIClient()
         response = client.get(reverse('snippet-list'))
