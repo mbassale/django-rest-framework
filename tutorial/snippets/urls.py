aa348b6c21a718cf436183e75ec7edded4aca2c8
Marco Bassaletti <bassaletti@gmail.com>
2020-07-04 21:05:45 -0400

    Highlight snippet endpoint.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -5,7 +5,8 @@ from tutorial.snippets import views
 urlpatterns = [
     path('', views.api_root),
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail')
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,10 +1,19 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from tutorial.snippets.models import Snippet
 from tutorial.snippets.serializers import SnippetSerializer
 from tutorial.snippets.permissions import IsOwnerOrReadOnly
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class SnippetList(generics.ListCreateAPIView):
